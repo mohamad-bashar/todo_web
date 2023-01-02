b334eb2e2be2a66a4d5a6f53530e5310b275b135
mohamad <mohamadbashar07@gmail.com>
2023-01-02 18:29:14 +0400

    initalize commit

---
+++ functions.py
@@ -0,0 +1,22 @@
+def convert_number_to_words(number):
+    ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen"]
+
+    if number < 10:
+        return ones[number]
+    elif number < 20:
+        return teens[number - 10]
+    else:
+        return tens[number // 10] + ones[number % 10]
+    
+def get_todos(filepath = r'.\todos.txt'):
+    with open(filepath, 'r') as file:
+        todos = file.readlines()  
+    return todos
+
+
+def write_todos(todos, filepath = r'.\todos.txt'):
+    with open(filepath, 'w') as file:
+        file.writelines(todos)   
